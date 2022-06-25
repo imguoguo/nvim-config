56fe89c77ba77a17a138bb65be40dc206629867a
Guoguo <i@qwq.trade>
2022-06-25 19:52:38 +0800

    Just Simple First Commit :)

---
+++ init.vim
@@ -0,0 +1,9 @@
+set number
+set relativenumber
+set autoindent
+set shiftwidth=4
+set mouse=a
+
+map <A-m> :NvimTreeToggle<CR>
+
+lua require('plugins')
---
+++ lua/plugins.lua
@@ -0,0 +1,36 @@
+return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'mfussenegger/nvim-dap'
+    use 'yamatsum/nvim-nonicons'
+    use 'luochen1990/rainbow'
+    use 'jiangmiao/auto-pairs'
+    use 'vim-airline/vim-airline-themes'
+    use 'vim-airline/vim-airline'
+    -- use 'Yggdroot/indentLine'
+
+    -- if use nvim-web-devicons
+    use {
+      'yamatsum/nvim-nonicons',
+      requires = {'kyazdani42/nvim-web-devicons'}
+    }
+    use 'sebdah/vim-delve'
+    use {
+	'tanvirtin/monokai.nvim',
+	config = function() require('monokai').setup() end
+    }
+
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = {
+	    'kyazdani42/nvim-web-devicons',
+	},
+	config = function() require('nvim-tree').setup() end
+    }
+
+    use {
+	'neoclide/coc.nvim',
+	branch = 'release'
+    }
+    -- Go related plugins
+    use 'fatih/vim-go'
+end)
